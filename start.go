1b02bf2f7c46936c7f431f50bc1a70060c96c007
urakawa-jinsei <urakawa_jinsei@retail-ai.jp>
2023-05-24 02:00:24 +0000

    [update]登録処理完了

--- start.go
+++ start.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"entdemo/ent"
 
 	_ "github.com/lib/pq"
 )
+
+func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.
+		Create().
+		SetAge(30).
+		SetName("a8m").
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %w", err)
+	}
+	log.Println("user was created: ", u)
+	return u, nil
+}
 
 func main() {
 	client, err := ent.Open("postgres", "host=localhost port=5432 user=user dbname=db password=pass")
@@ -19,4 +33,12 @@ func main() {
 	if err := client.Schema.Create(context.Background()); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
+
+	// CreateUserを呼び出す
+	ctx := context.Background()
+	user, err := CreateUser(ctx, client)
+	if err != nil {
+		log.Fatalf("failed to create user: %v", err)
+	}
+	fmt.Println("User created:", user)
 }
